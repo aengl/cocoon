fd8fd271bfd9b2a0cf8f2035642e48b4e2ba69b2
Lynn Smeria <ae@cephea.de>
2018-10-08 18:53:00 +0200

    Create tree from definitions

--- core/definitions.ts
+++ core/definitions.ts
@@ -34,8 +34,7 @@ export interface CocoonDefinitions {
 export interface CocoonNode extends NodeConfiguration {
   type: string;
   group: string;
-  edgesIn: CocoonEdge[];
-  edgesOut: CocoonEdge[];
+  edges: CocoonEdge[];
 }
 
 export interface CocoonEdge {
@@ -52,25 +51,52 @@ export function loadDefinitionFromFile(definitionPath: string) {
 export function listNodesInDefinitions(
   definitions: CocoonDefinitions
 ): CocoonNode[] {
+  // Create a flat list of nodes
   const groups = Object.keys(definitions);
   const nodes = _.flatten(
     groups.map(group => {
-      const nodes = definitions[group].nodes;
-      return nodes.map(node => {
+      return definitions[group].nodes.map(node => {
         const type = Object.keys(node)[0];
         return {
-          type,
+          edges: [] as CocoonEdge[],
           group,
-          edgesIn: [],
-          edgesOut: [],
+          type,
           ...node[type],
         };
       });
     })
   );
-  return nodes.map(node => assignEdges(node, nodes));
+
+  // Map all nodes
+  const nodeMap = nodes.reduce((all, node) => {
+    all[node.id] = node;
+    return all;
+  }, {});
+
+  // Assign edges to nodes
+  nodes.forEach(node => {
+    const nodeOut = node.out;
+    if (nodeOut !== undefined) {
+      node.edges = Object.keys(nodeOut).map(key => {
+        const { id, port } = parsePortDefinition(nodeOut[key]);
+        return {
+          from: node,
+          fromPort: key,
+          to: nodeMap[id],
+          toPort: port,
+        };
+      });
+    }
+  });
+
+  return nodes;
 }
 
-function assignEdges(node: CocoonNode, nodes: CocoonNode[]) {
-  return node;
+function parsePortDefinition(definition: string) {
+  const match = definition.match(/(?<id>[^/]+)\/(?<port>.+)/);
+  if (!match) {
+    throw new Error(`Invalid port definition: "${definition}"`);
+  }
+  const groups: any = match.groups;
+  return { id: groups.id, port: groups.port };
 }
