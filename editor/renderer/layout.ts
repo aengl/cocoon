5a71c126386dcae871480ee057510d6cdf5f5f7a
Lynn Smeria <ae@cephea.de>
2018-10-13 13:27:33 +0200

    Improve layouting algorithm

--- editor/renderer/layout.ts
+++ editor/renderer/layout.ts
@@ -1,15 +1,37 @@
 import { CocoonNode } from '../../core/graph';
 
 export function assignXY(nodes: CocoonNode[]) {
-  let x = 0;
-  let y = 0;
+  // Find starting node
+  const startNode = nodes.find(node => node.edgesIn.length === 0);
+  positionNode(startNode, 0, 0);
 
-  nodes.forEach(node => {
-    node.definition.x = x;
-    node.definition.y = y;
-
-    x += 1;
-  });
+  // Recursively position connected nodes
+  positionConnectedNodes(startNode, nodes);
 
   return nodes;
+}
+
+function positionConnectedNodes(node: CocoonNode, nodes: CocoonNode[]) {
+  const x = node.definition.x;
+  const y = node.definition.y;
+
+  // Find nodes that share an edge with the current node
+  const connectedNodes = nodes.filter(
+    n =>
+      n.edgesIn.find(e => e.from.definition.id === node.definition.id) !==
+      undefined
+  );
+
+  // Position all connected nodes in a single column, next to the current node
+  if (connectedNodes) {
+    connectedNodes.forEach((n, i) => {
+      positionNode(n, x + 1, y + i);
+      positionConnectedNodes(n, nodes);
+    });
+  }
+}
+
+function positionNode(node: CocoonNode, x: number, y: number) {
+  node.definition.x = node.definition.x === undefined ? x : node.definition.x;
+  node.definition.y = node.definition.y === undefined ? y : node.definition.y;
 }
