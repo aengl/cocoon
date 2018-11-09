249cbe86e5aaa39507883253f8bf8ca41a21deb1
Lynn Smeria <ae@cephea.de>
2018-11-09 09:44:16 +0100

    Domain: parse numbers and quantities

--- core/nodes/data/Domain.ts
+++ core/nodes/data/Domain.ts
@@ -1,3 +1,4 @@
+import Qty from 'js-quantities';
 import _ from 'lodash';
 import { ICocoonNode, listDimensions, NodeContext } from '..';
 import { parseYamlFile } from '../../fs';
@@ -48,8 +49,15 @@ export { Domain };
 
 interface DomainDimension {
   name: string;
-  type?: 'string' | 'number' | 'number_with_unit';
+  type?: 'string' | 'number' | 'quantity';
   match: string[];
+  replace?: Array<[string, string]>;
+
+  // quantity
+  unit?: string;
+
+  // discreet
+  values?: { [value: string]: string[] };
 }
 
 type Domain = DomainDimension[];
@@ -74,12 +82,51 @@ function processDimension(
       `Data dimension "${matchingDimensionName}" matches "${dimension.name}"`
     );
 
-    // Normalise dimension name
+    // Normalise dimension name and values
     data.forEach(item => {
       if (item[matchingDimensionName] !== undefined) {
-        item[dimension.name] = item[matchingDimensionName];
+        const v = parseValue(item[matchingDimensionName], dimension, debug);
         delete item[matchingDimensionName];
+        if (_.isNil(v)) {
+          delete item[dimension.name];
+        } else {
+          item[dimension.name] = v;
+        }
       }
     });
   }
+}
+
+function normaliseNumber(v: any) {
+  if (_.isString(v)) {
+    return v.replace(/([0-9]),([0-9])/g, '$1.$2');
+  }
+  return v;
+}
+
+function parseValue(
+  v: any,
+  dimension: DomainDimension,
+  debug: NodeContext['debug']
+) {
+  if (dimension.replace !== undefined) {
+    v = dimension.replace.reduce((x: string, y) => x.replace(y[0], y[1]), v);
+  }
+  switch (dimension.type) {
+    case 'number': {
+      const n = parseFloat(normaliseNumber(v));
+      return _.isNaN(n) ? null : n;
+    }
+    case 'quantity': {
+      try {
+        return _.isNil(v)
+          ? null
+          : new Qty(normaliseNumber(v)).to(dimension.unit!).scalar;
+      } catch (error) {
+        debug(error.message, v);
+        return null;
+      }
+    }
+  }
+  return v;
 }
