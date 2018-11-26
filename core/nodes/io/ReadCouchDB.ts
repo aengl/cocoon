1448cac2b62cfd2d173f8eca3b1af5de36e0727f
Lynn Smeria <ae@cephea.de>
2018-11-26 17:33:26 +0100

    ReadCouchDB: support Mango queries

--- core/nodes/io/ReadCouchDB.ts
+++ core/nodes/io/ReadCouchDB.ts
@@ -1,17 +1,42 @@
 import got from 'got';
 import { NodeObject, readPersistedCache, writePersistedCache } from '..';
 
+export interface CouchDBRow {
+  id: string;
+  key: string;
+  value: {
+    rev: string;
+  };
+  doc?: object;
+}
+
+export interface CouchDBDocument {
+  _id: string;
+  _rev: string;
+  [key: string]: any;
+}
+
+export interface CouchDBResponse {
+  offset: number;
+  rows: CouchDBRow[];
+  total_rows: number;
+}
+
+export interface CouchDBQueryResponse {
+  bookmark: string;
+  docs: CouchDBDocument[];
+  warning: string;
+}
+
 /**
  * Imports databases from CouchDB.
  */
 const ReadCouchDB: NodeObject = {
   in: {
-    config: {
-      defaultValue: {},
-    },
     database: {
       required: true,
     },
+    query: {},
     url: {},
   },
 
@@ -21,6 +46,7 @@ const ReadCouchDB: NodeObject = {
 
   process: async context => {
     const { node } = context;
+
     // Try to get data from persisted cache
     const persistedData = await readPersistedCache<object[]>(node, 'data');
     if (persistedData !== undefined) {
@@ -31,23 +57,48 @@ const ReadCouchDB: NodeObject = {
     // Request from database
     const url = context.readFromPort<string>('url', 'http://localhost:5984');
     const database = context.readFromPort<string>('database');
-    const requestUrl = `${url}/${database}/_all_docs?include_docs=true`;
-    context.debug(`fetching "${requestUrl}"`);
-    const response = await got(requestUrl, {
-      json: true,
-      ...context.readFromPort<object>('config'),
-    });
-    if (!response.statusCode) {
-      throw Error(`request failed`);
+    const query = context.readFromPort<object>('query');
+    let data: object[];
+    if (query !== undefined) {
+      const requestUrl = `${url}/${database}/_find`;
+      context.debug(`querying "${requestUrl}"`);
+      const response: got.Response<CouchDBQueryResponse> = await got(
+        requestUrl,
+        {
+          body: query,
+          json: true,
+          method: 'POST',
+        }
+      );
+      checkResponse(response);
+      data = response.body.docs;
+    } else {
+      const requestUrl = `${url}/${database}/_all_docs?include_docs=true`;
+      context.debug(`fetching "${requestUrl}"`);
+      const response: got.Response<CouchDBResponse> = await got(requestUrl, {
+        json: true,
+      });
+      checkResponse(response);
+      data = response.body.rows.map(item => item.doc!);
     }
-    if (response.statusCode >= 400) {
-      throw Error(`request failed with status ${response.statusCode}`);
-    }
-    const data = response.body.rows.map(item => item.doc);
+
+    // Write data
     context.writeToPort<object[]>('data', data);
+
+    // Persist data
     await writePersistedCache(node, 'data', data);
-    return `Imported ${response.body.total_rows} documents`;
+
+    return `Imported ${data.length} documents`;
   },
 };
 
 export { ReadCouchDB };
+
+async function checkResponse(response: got.Response<any>) {
+  if (!response.statusCode) {
+    throw Error(`request failed`);
+  }
+  if (response.statusCode >= 400) {
+    throw Error(`request failed with status ${response.statusCode}`);
+  }
+}
