e51a9aad0f41ba993b2d45b0b19c9e6aa91a2028
Lynn Smeria <ae@cephea.de>
2018-11-30 11:58:32 +0100

    FilterCustom: support various filter definition formats

--- core/nodes/filter/FilterCustom.tsx
+++ core/nodes/filter/FilterCustom.tsx
@@ -1,6 +1,12 @@
+import _ from 'lodash';
 import { NodeObject } from '../../../common/node';
 
 export type FilterFunction = (item: object) => boolean;
+export type FilterDefinition =
+  | string
+  | string[]
+  | FilterFunction
+  | FilterFunction[];
 
 const FilterCustom: NodeObject = {
   in: {
@@ -21,11 +27,15 @@ const FilterCustom: NodeObject = {
 
   async process(context) {
     const data = context.readFromPort<object[]>('data');
-    const filterList = context.readFromPort<FilterFunction[]>('filter');
+    const filterList = _.castArray<string | FilterFunction>(
+      context.readFromPort<FilterDefinition>('filter')
+    );
 
     let selectedData = data;
     for (const filter of filterList) {
-      selectedData = selectedData.filter(item => Boolean(filter(item)));
+      // tslint:disable-next-line no-eval
+      const f = _.isString(filter) ? eval(filter) : filter;
+      selectedData = selectedData.filter(item => Boolean(f(item)));
     }
     context.writeToPort('data', selectedData);
     return `Filtered out ${data.length - selectedData.length} items`;
