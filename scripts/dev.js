3119428671e9da4b6697969b79bd4cdbb8ae2903
Lynn Smeria <ae@cephea.de>
2019-01-07 19:30:37 +0100

    Add missing script

---
+++ scripts/dev.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+require('debug').enable('core:*,main:*,common:*');
+const concurrently = require('concurrently');
+const path = process.argv[2] || '~/Resilio Sync/Tibi/Cocoon2/test.yml';
+concurrently(
+  [
+    {
+      command: `tsc --watch 1>/dev/null`,
+      name: 'compiler',
+    },
+    {
+      command: `node --inspect=9340 -r esm editor/main.js "${path}"`,
+      name: 'editor',
+    },
+    {
+      command: `webpack-dev-server`,
+      name: 'webpack',
+    },
+  ],
+  {
+    raw: true,
+    killOthers: ['failure', 'success'],
+  }
+);
