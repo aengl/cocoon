14b3669ee5fa52fa69e2458a146ac84fc5c4c57e
Lynn Smeria <ae@cephea.de>
2019-01-31 15:18:42 +0100

    Editor: add flag for specifying browser executable

--- editor/main-browser.ts
+++ editor/main-browser.ts
@@ -1,8 +1,17 @@
+import { exec } from 'child_process';
 import opn from 'opn';
 import { initialise } from './main-common';
 import { createURI } from './uri';
 
-export async function initialiseBrowser(definitionsPath?: string) {
+export async function initialiseBrowser(
+  definitionsPath?: string,
+  browserPath?: string
+) {
   await initialise();
-  opn(createURI('editor.html', { definitionsPath }));
+  const uri = createURI('editor.html', { definitionsPath });
+  if (browserPath) {
+    exec(`"${browserPath}" "${uri}"`);
+  } else {
+    opn(uri);
+  }
 }
--- editor/main.ts
+++ editor/main.ts
@@ -5,11 +5,12 @@ import { initialiseCarlo } from './main-carlo';
 program
   .argument('<yml>', 'Path to the Cocoon definition file')
   .option('-c, --carlo', 'Run the editor in Carlo')
+  .option('-b, --browser', 'Path to the browser executable')
   .action(async (args, options) => {
     if (options.carlo) {
       await initialiseCarlo(args.yml);
     } else {
-      await initialiseBrowser(args.yml);
+      await initialiseBrowser(args.yml, options.browser);
     }
   });
 
