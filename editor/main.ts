f15aaff904441f4c680c9d0106a1a55e3cddb7b6
Lynn Smeria <ae@cephea.de>
2019-02-10 15:12:41 +0100

    Editor: add `--headless` flag

--- editor/main.ts
+++ editor/main.ts
@@ -1,15 +1,19 @@
 import program from 'caporal';
 import { initialiseBrowser } from './main-browser';
 import { initialiseCarlo } from './main-carlo';
+import { initialise } from './main-common';
 
 program
-  .argument('<yml>', 'Path to the Cocoon definition file')
+  .argument('[yml]', 'Path to the Cocoon definition file')
   .option('-c, --carlo', 'Run the editor in Carlo')
   .option('-b, --browser', 'Path to the browser executable')
   .option('--canary', 'Open editor in Google Canary')
+  .option('--headless', 'Run the editor headlessly')
   .action(async (args, options) => {
     if (options.carlo) {
       await initialiseCarlo(args.yml);
+    } else if (options.headless) {
+      await initialise();
     } else {
       await initialiseBrowser(
         args.yml,
