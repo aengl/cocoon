c4080cfa816b979c14dd9c78a9a3bd134c4aef8e
Lynn Smeria <ae@cephea.de>
2019-05-06 22:36:30 +0900

    Improve scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-find common/ core/ editor/ -name '*.js' ! -name 'bin.js' -delete
+find common/ core/ editor/ -name '*.js' ! -name 'bin.js' ! -name 'dev.js' ! -name 'http-server.js' -delete
 find common/ core/ editor/ -name '*.map' -delete
