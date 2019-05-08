e5ade592b94a63e05b3626f09b3b83fb5a90378d
Lynn Smeria <ae@cephea.de>
2019-05-08 17:58:17 +0900

    Bundle dependencies (except lodash and react)

--- plugins/echarts/rollup.config.js
+++ plugins/echarts/rollup.config.js
@@ -1,15 +1,38 @@
 import commonjs from 'rollup-plugin-commonjs';
+import externalGlobals from 'rollup-plugin-external-globals';
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript';
 
 export default {
   input: './src/components/index.ts',
   plugins: [
+    terser(),
     typescript(),
+    resolve({
+      browser: true,
+      only: ['echarts', 'simple-statistics'],
+    }),
     commonjs({
-      include: 'node_modules/css-element-queries/**',
+      sourceMap: false,
+      namedExports: {
+        'node_modules/react/index.js': ['useRef', 'useEffect'],
+        'node_modules/simple-statistics/dist/simple-statistics.min.js': [
+          'interquartileRange',
+        ],
+      },
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    externalGlobals({
+      react: 'React',
+      lodash: '_',
     }),
   ],
   output: {
+    compact: true,
     file: 'dist/components.js',
     format: 'esm',
   },
