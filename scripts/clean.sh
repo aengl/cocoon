819a132996b9c9afe83ccfb7189c91050f1854ab
Lynn Smeria <ae@cephea.de>
2019-05-28 21:21:18 +0900

    Fix clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-find common/ cocoon-monaco/ cocoon-node/ core/ editor/ -name '*.js' ! -name 'bin.js' ! -name 'dev.js' -delete
+find common/ core/ editor/ -name '*.js' ! -name 'bin.js' -delete
 find common/ core/ editor/ -name '*.map' -delete
+rm cocoon-monaco/*.js cocoon-node/*.js
+rm cocoon-monaco/*.map cocoon-node/*.map
