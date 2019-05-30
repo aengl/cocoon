6dd19585a80cac4cf54243d58d3ead6c189b0654
Lynn Smeria <ae@cephea.de>
2019-05-30 09:27:48 +0900

    Use `@cocoon/rollup`

--- plugins/echarts/rollup.config.js
+++ plugins/echarts/rollup.config.js
@@ -1,42 +1,8 @@
-import commonjs from 'rollup-plugin-commonjs';
-import externalGlobals from 'rollup-plugin-external-globals';
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript';
+import { createComponentConfig } from '@cocoon/rollup';
 
-const isDev = process.env.DEBUG !== undefined;
-
-const productionPlugins = [
-  terser(),
-  replace({
-    'process.env.NODE_ENV': JSON.stringify('production'),
-  }),
-];
-
-export default {
-  input: './src/components/index.ts',
-  plugins: [
-    ...[
-      typescript(),
-      resolve({
-        browser: true,
-        only: ['echarts', 'd3-array'],
-      }),
-      commonjs({
-        sourceMap: false,
-      }),
-      externalGlobals({
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        lodash: '_',
-      }),
-    ],
-    ...(isDev ? [] : productionPlugins),
-  ],
-  output: {
-    compact: !isDev,
-    file: 'dist/components.js',
-    format: 'esm',
+export default createComponentConfig({
+  production: !process.env.DEBUG,
+  resolveConfig: {
+    only: ['echarts', 'd3-array'],
   },
-};
+});
