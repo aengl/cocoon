1772f63cb013186cf4e5cbd6f91508b24897c237
Lynn Smeria <ae@cephea.de>
2019-05-30 09:15:59 +0900

    Use `@cocoon/rollup`

--- plugins/views/rollup.config.js
+++ plugins/views/rollup.config.js
@@ -1,49 +1,14 @@
-import commonjs from 'rollup-plugin-commonjs';
-import externalGlobals from 'rollup-plugin-external-globals';
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript';
+import { createComponentConfig } from '@cocoon/rollup';
 
-const isDev = process.env.DEBUG !== undefined;
-
-const productionPlugins = [
-  terser(),
-  replace({
-    'process.env.NODE_ENV': JSON.stringify('production'),
-  }),
-];
-
-export default {
-  input: './src/components/index.ts',
-  plugins: [
-    ...[
-      typescript(),
-      resolve({
-        browser: true,
-      }),
-      commonjs({
-        sourceMap: false,
-        namedExports: {
-          'node_modules/react-is/index.js': [
-            'ForwardRef',
-            'isElement',
-            'isValidElementType',
-          ],
-        },
-      }),
-      externalGlobals({
-        lodash: '_',
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        'styled-components': 'styled',
-      }),
-    ],
-    ...(isDev ? [] : productionPlugins),
-  ],
-  output: {
-    compact: !isDev,
-    file: 'dist/components.js',
-    format: 'esm',
+export default createComponentConfig({
+  production: !process.env.DEBUG,
+  commonjsConfig: {
+    namedExports: {
+      'node_modules/react-is/index.js': [
+        'ForwardRef',
+        'isElement',
+        'isValidElementType',
+      ],
+    },
   },
-};
+});
