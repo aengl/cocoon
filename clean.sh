7af05f089f25fbd16fc21ca369adf9f248b4d68f
Lynn Smeria <ae@cephea.de>
2019-06-26 17:25:50 +0900

    Minor fixes & cleanup

--- clean.sh
+++ clean.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-find cocoon/ cocoon-editor/ cocoon-shared/ -name '*.js' -delete
-find cocoon/ cocoon-editor/ cocoon-shared/ -name '*.map' -delete
-find cocoon/ cocoon-editor/ cocoon-shared/ -name '*.d.ts' -delete
-find cocoon/ cocoon-editor/ cocoon-shared/ -name '*.tsbuildinfo' -delete
+
+# Remove build artefacts
+find cocoon/ cocoon-editor/ cocoon-monaco/ cocoon-shared/ cocoon-testing/ -name '*.js' -delete
+find cocoon/ cocoon-editor/ cocoon-monaco/ cocoon-shared/ cocoon-testing/ -name '*.d.ts' -delete
+find . -name '*.map' -delete
+find . -name '*.tsbuildinfo' -delete
+
+# Remove node module folders
+find . -type d -name node_modules -exec rm -rf {} \;
+
+# Remove dist folders
+find . -type d -name dist -exec rm -rf {} \;
