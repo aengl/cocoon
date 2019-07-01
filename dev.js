a61c4aa1fff7a278a3d111a8389a2806df9e1619
Lynn Smeria <ae@cephea.de>
2019-07-01 17:59:26 +0900

    Add `--with-plugins` flag to dev script

--- dev.js
+++ dev.js
@@ -1,17 +1,23 @@
 #!/usr/bin/env node
-
 const concurrently = require('concurrently');
 
-concurrently(
+const commands = [
+  {
+    command: `npx lerna exec --parallel --stream --scope "@cocoon/@(cocoon|editor|shared|types|util)" -- yarn dev 1>/dev/null`,
+    name: 'dev',
+  },
+  {
+    command: `npx lerna exec --parallel --stream --scope "@cocoon/editor" -- yarn dev:editor-ui`,
+    name: 'ui',
+  },
+  {
+    command: `sleep 15 && yarn dev:editor`,
+    name: 'nodemon',
+  },
+];
+
+if (process.argv[2] === '--with-plugins') {
   [
-    {
-      command: `npx lerna exec --parallel --stream --scope "@cocoon/@(cocoon|editor|shared|types|util)" -- yarn dev 1>/dev/null`,
-      name: 'dev',
-    },
-    {
-      command: `npx lerna exec --parallel --stream --scope "@cocoon/editor" -- yarn dev:editor-ui`,
-      name: 'ui',
-    },
     {
       command: `sleep 30 && npx lerna exec --parallel --stream --scope "@cocoon/plugin-*" -- yarn dev:ncc`,
       name: 'plugins/nodes',
@@ -20,15 +26,12 @@ concurrently(
       command: `sleep 30 && npx lerna exec --parallel --stream --scope "@cocoon/plugin-*" -- yarn dev:rollup`,
       name: 'plugins/components',
     },
-    {
-      command: `sleep 15 && yarn dev:editor`,
-      name: 'nodemon',
-    },
-  ],
-  {
-    raw: true,
-    killOthers: ['failure', 'success'],
-  }
-).catch(() => {
+  ].forEach(x => commands.push(x));
+}
+
+concurrently(commands, {
+  raw: true,
+  killOthers: ['failure', 'success'],
+}).catch(() => {
   process.exit(0);
 });
