8dfbad29929de80e68cbe88125d55040ed95c195
Lynn Smeria <ae@cephea.de>
2019-07-25 16:29:58 +0800

    DataView: optimise rendering

--- packages/editor/src/ui/DataView.tsx
+++ packages/editor/src/ui/DataView.tsx
@@ -6,7 +6,6 @@ import {
 } from '@cocoon/ipc';
 import {
   CocoonRegistry,
-  CocoonView,
   CocoonViewComponent,
   CocoonViewProps,
   GraphNode,
@@ -14,7 +13,7 @@ import {
 } from '@cocoon/types';
 import requireCocoonView from '@cocoon/util/requireCocoonView';
 import Debug from 'debug';
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { ErrorPage } from './ErrorPage';
 import { importViewComponent } from './modules';
@@ -32,8 +31,6 @@ export interface DataViewProps {
 export const DataView = memo(DataViewComponent, viewPropsAreEqual);
 
 function DataViewComponent(props: DataViewProps) {
-  // TODO: view renders multiple times despite memo() for some reason
-  // debug('rendering view', props);
   const {
     height,
     isPreview,
@@ -43,24 +40,14 @@ function DataViewComponent(props: DataViewProps) {
     viewDataId,
     width,
   } = props;
-  const viewName = node.view;
-  if (!viewName) {
-    return null;
-  }
+  const viewName = node.view!;
+  const view = requireCocoonView(registry, viewName);
 
   const [error, setError] = useState<Error | null>(null);
   const [viewData, setViewData] = useState(null);
   const [viewComponent, setViewComponent] = useState<{
     value: CocoonViewComponent;
   } | null>(null);
-
-  // Resolve view
-  let view: CocoonView;
-  try {
-    view = requireCocoonView(registry, viewName);
-  } catch (error) {
-    return renderError(error, isPreview);
-  }
 
   // Query view data
   useEffect(() => {
@@ -69,84 +56,86 @@ function DataViewComponent(props: DataViewProps) {
     });
   }, [viewDataId]);
 
-  // Handle error & missing data
-  if (error) {
-    return renderError(error, isPreview);
-  }
-  if (!viewData) {
-    return null;
-  }
-
   // Resolve view component
   //
   // We do this as late as possible, so that potentially expensive bundle
   // imports are done only when a view is rendered with available data)
-  if (!viewComponent) {
+  if (view && viewData && !viewComponent) {
     const resolve = async () => {
       const value = await importViewComponent(view, viewName);
       setViewComponent({ value });
     };
     resolve();
-    return null;
   }
 
-  // Create view props
-  const viewDebug = Debug(`editor:${node.id}`);
-  const supportedViewStates = node.cocoonNode!.supportedViewStates;
-  const viewProps: CocoonViewProps = {
-    context: {
-      debug: viewDebug,
-      graphNode: node,
-      height,
-      isPreview,
-      node: {
-        id: node.id,
-        supportedViewStates,
-        supportsViewState: key =>
-          supportedViewStates ? supportedViewStates.indexOf(key) >= 0 : false,
+  // Despite memo(), the view component will still render itself at least twice
+  // for new view data, first when receiving the viewDataId, then after having
+  // fetched the actual data via IPC. To spare the actual view component from an
+  // extra render, we memo it on the data object.
+  const renderedViewComponent = useMemo(() => {
+    if (!viewData || !viewComponent) {
+      return null;
+    }
+    const viewDebug = Debug(`editor:${node.id}`);
+    const supportedViewStates = node.cocoonNode!.supportedViewStates;
+    return React.createElement(viewComponent.value, {
+      context: {
+        debug: viewDebug,
+        graphNode: node,
+        height,
+        isPreview,
+        node: {
+          id: node.id,
+          supportedViewStates,
+          supportsViewState: key =>
+            supportedViewStates ? supportedViewStates.indexOf(key) >= 0 : false,
+        },
+        query: (query, callback) => {
+          viewDebug(`querying data`, query);
+          sendQueryNodeView({ nodeId: node.id, query }, args => {
+            viewDebug(`got data query response`, query);
+            callback(args);
+          });
+        },
+        search,
+        send: data => {
+          sendToNode({
+            data,
+            nodeId: node.id,
+          });
+        },
+        syncViewState: viewState => {
+          if (Object.keys(viewState).length > 0) {
+            // In order to conveniently filter unsupported view states we may
+            // sometimes call this method with an empty state object. Those
+            // calls can safely be ignored.
+            viewDebug(`view state changed`, viewState);
+            sendChangeNodeViewState({ nodeId: node.id, viewState });
+          }
+        },
+        viewData,
+        viewPort: node.viewPort!,
+        viewState: node.definition.viewState || {},
+        width,
       },
-      query: (query, callback) => {
-        viewDebug(`querying data`, query);
-        sendQueryNodeView({ nodeId: node.id, query }, args => {
-          viewDebug(`got data query response`, query);
-          callback(args);
-        });
-      },
-      search,
-      send: data => {
-        sendToNode({
-          data,
-          nodeId: node.id,
-        });
-      },
-      syncViewState: viewState => {
-        if (Object.keys(viewState).length > 0) {
-          // In order to conveniently filter unsupported view states we may
-          // sometimes call this method with an empty state object. Those
-          // calls can safely be ignored.
-          viewDebug(`view state changed`, viewState);
-          sendChangeNodeViewState({ nodeId: node.id, viewState });
-        }
-      },
-      viewData,
-      viewPort: node.viewPort!,
-      viewState: node.definition.viewState || {},
-      width,
-    },
-  };
+    });
+  }, [viewData, viewComponent]);
+
+  // Handle error & missing data
+  if (error) {
+    return renderError(error, isPreview);
+  }
 
   return isPreview ? (
     <PreviewWrapper style={{ height, width }}>
-      {React.createElement(viewComponent.value, viewProps)}
+      {renderedViewComponent}
     </PreviewWrapper>
   ) : (
-    <Wrapper style={{ height, width }}>
-      {React.createElement(viewComponent.value, viewProps)}
-    </Wrapper>
+    <Wrapper style={{ height, width }}>{renderedViewComponent}</Wrapper>
   );
 }
 
-function viewPropsAreEqual(prevProps, nextProps) {
+function viewPropsAreEqual(prevProps: DataViewProps, nextProps: DataViewProps) {
   // Only update the state when the node is fully processed -- otherwise the
   // status sync at the beginning of the node evaluation will erase the
   // virtual dom for the visualisation (since no view data is available at
