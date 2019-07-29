9a7b6ec6808c4a796d88dd335bdaf9794f5ea19c
Lynn Smeria <ae@cephea.de>
2019-07-29 19:17:53 +0700

    views: remove `Table`

--- packages/plugins/views/components/index.ts
+++ packages/plugins/views/components/index.ts
@@ -1,3 +1,2 @@
 export { HTML } from './HTML';
 export { Image } from './Image';
-export { Table } from './Table';
--- packages/plugins/views/index.ts
+++ packages/plugins/views/index.ts
@@ -1,3 +1,2 @@
 export { HTML } from './views/HTML';
 export { Image } from './views/Image';
-export { Table } from './views/Table';
