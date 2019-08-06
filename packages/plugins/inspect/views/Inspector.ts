cbdb9a97966d602ca97399500d7c9094fc06bb5a
Lynn Smeria <ae@cephea.de>
2019-08-06 20:55:32 +0700

    Inspector: add `maxSize` configuration

--- packages/plugins/inspect/views/Inspector.ts
+++ packages/plugins/inspect/views/Inspector.ts
@@ -7,6 +7,7 @@ export interface ViewState {
   expandLevel?: number;
   expandPaths?: string | string[];
   limit?: number;
+  maxSize?: number;
   preview?: string | string[];
 }
 
@@ -18,16 +19,22 @@ export const Inspector: CocoonView<Data, ViewState> = {
     expandLevel: `See https://github.com/storybookjs/react-inspector#api`,
     expandPaths: `See https://github.com/storybookjs/react-inspector#api`,
     limit: `If the data is an array, limits the number of items that are shown in the inspector. (default: \`100\`)`,
+    maxSize: `The maximum allowed serialised data size in bytes. (default: 10 000 000)`,
     preview: `List of attributes to preview inline when summarising objects.`,
   },
 
   serialiseViewData: async (context, data: object[], state) => {
-    const limit = state.limit === undefined ? 100 : state.limit;
+    const limit = state.limit || 100;
+    const maxSize = state.maxSize || 10_000_000;
     const serialisedData = JSON.stringify(
-      data.length > limit ? data.slice(0, 100) : data
+      data.length > limit ? data.slice(0, limit) : data
     );
-    if (serialisedData.length > 10_000_000) {
-      throw new Error(`Inspector received too much data`);
+    if (serialisedData.length > maxSize) {
+      throw new Error(
+        `Inspector received too much data (${serialisedData.length /
+          1024 /
+          1024} MB)`
+      );
     }
     return serialisedData;
   },
