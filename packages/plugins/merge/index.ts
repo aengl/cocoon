3155c8b57496bc5839a4dee15545a472c380d8e0
Lynn Smeria <ae@cephea.de>
2019-08-06 22:49:25 +0700

    Remove `merge` plugin

--- packages/plugins/merge/index.ts
+++
@@ -1,4 +0,0 @@
-export { Match } from './nodes/Match';
-export { MatchAndMerge } from './nodes/MatchAndMerge';
-export { Merge } from './nodes/Merge';
-export { MergeDiff } from './views/MergeDiff';
