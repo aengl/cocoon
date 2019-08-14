830e977317fb5ddc8403e12f4756aa1c5122d42d
Lynn Smeria <ae@cephea.de>
2019-08-14 21:30:54 +0200

    Fix: cache not clearing for paths without extension

--- packages/cocoon/src/registry.ts
+++ packages/cocoon/src/registry.ts
@@ -132,7 +132,9 @@ async function importFromModule(
   componentPath?: string
 ) {
   const time = process.hrtime();
-  delete require.cache[modulePath];
+  delete require.cache[
+    modulePath.endsWith('.js') ? modulePath : `${modulePath}.js`
+  ];
   const moduleExports = require(modulePath);
   const diff = process.hrtime(time);
   const importTimeInMs = diff[0] * 1e3 + Math.round(diff[1] / 1e6);
