495a10d6ff7dbb53eb7b4aca59f09f6ffc7436c3
Lynn Smeria <ae@cephea.de>
2019-08-14 22:13:33 +0200

    Only build packages with changes when releasing

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash -e
 yarn
 yarn bootstrap
-npx lerna exec --scope "@cocoon/types" -- yarn build
-npx lerna exec --scope "@cocoon/util" -- yarn build
-npx lerna exec --scope "@cocoon/cocoon" -- yarn build
-npx lerna exec --scope "@cocoon/monaco" -- yarn build
-npx lerna exec --scope "@cocoon/testing" -- yarn build
-npx lerna exec --scope "@cocoon/@(plugin-*)" -- yarn build
-npx lerna exec --scope "@cocoon/editor" -- yarn build
-npx lerna exec --scope "@cocoon/docs" -- yarn build
+npx lerna run build --scope "@cocoon/types" $1
+npx lerna run build --scope "@cocoon/util" $1
+npx lerna run build --scope "@cocoon/cocoon" $1
+npx lerna run build --scope "@cocoon/monaco" $1
+npx lerna run build --scope "@cocoon/testing" $1
+npx lerna run build --scope "@cocoon/@(plugin-*)" $1
+npx lerna run build --scope "@cocoon/editor" $1
+npx lerna run build --scope "@cocoon/docs" $1
