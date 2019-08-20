85c48a5612e0884234b65b6fa59b121af6c20f8c
Lynn Smeria <ae@cephea.de>
2019-08-20 15:17:49 +0200

    Fix Monaco editor debugging

--- packages/monaco/webpack.config.dev.js
+++ packages/monaco/webpack.config.dev.js
@@ -2,7 +2,6 @@ const config = require('./webpack.config');
 
 config.mode = 'development';
 config.devtool = 'inline-source-map';
-config.output.path = __dirname;
 config.module.rules.push({
   enforce: 'pre',
   exclude: /node_modules/,
