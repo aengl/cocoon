c4aeff2c069b6d64312c57e89ce06b68b43e9389
Lynn Smeria <ae@cephea.de>
2019-08-23 12:40:13 +0200

    Console: log errors

--- packages/editor/src/ui/Console.tsx
+++ packages/editor/src/ui/Console.tsx
@@ -6,37 +6,45 @@ import styled from 'styled-components';
 import { ipcContext } from './ipc';
 import { theme } from './theme';
 
+const isLogArgs = (x: LogArgs | Error): x is LogArgs => 'namespace' in x;
+
 export interface ChromeMemoryUsage {
   jsHeapSizeLimit: number;
   totalJSHeapSize: number;
   usedJSHeapSize: number;
 }
 
+export type Message = LogArgs | Error;
+
 export function Console() {
   const ipc = ipcContext();
 
-  const messagesRef = useRef<LogArgs[]>([]);
+  const messagesRef = useRef<Message[]>([]);
   const fadeTimer = useRef<number | null>(null);
   const [visible, setVisible] = useState<boolean>(false);
-  const [messages, setMessages] = useState<LogArgs[]>([]);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const logAndShowConsole = (message: Message) => {
+    while (messagesRef.current.length > 4) {
+      messagesRef.current.shift();
+    }
+    messagesRef.current.push(message);
+    setVisible(true);
+    setMessages([...messagesRef.current]);
+    if (fadeTimer.current) {
+      clearTimeout(fadeTimer.current);
+    }
+    fadeTimer.current = setTimeout(() => {
+      fadeTimer.current = null;
+      messagesRef.current = [];
+      setVisible(false);
+    }, 4000);
+  };
 
   useEffect(() => {
     const logHandler = log.register(ipc, args => {
       Debug(args.namespace)(args.message, ...args.additionalArgs);
-      while (messagesRef.current.length > 4) {
-        messagesRef.current.shift();
-      }
-      messagesRef.current.push(args);
-      setVisible(true);
-      setMessages([...messagesRef.current]);
-      if (fadeTimer.current) {
-        clearTimeout(fadeTimer.current);
-      }
-      fadeTimer.current = setTimeout(() => {
-        fadeTimer.current = null;
-        messagesRef.current = [];
-        setVisible(false);
-      }, 4000);
+      logAndShowConsole(args);
     });
 
     const errorHandler = error.register(ipc, args => {
@@ -44,6 +52,7 @@ export function Console() {
         const err = new Error(args.error.message);
         err.stack = args.error.stack;
         console.error(err);
+        logAndShowConsole(err);
       }
     });
 
@@ -55,19 +64,33 @@ export function Console() {
 
   return (
     <Wrapper style={{ opacity: visible ? 1 : 0 }}>
-      {messages.map((x, i) => (
-        <div key={i}>
-          <span
-            style={{
-              color: x.color,
-              marginRight: 2,
-            }}
-          >
-            {x.namespace}
-          </span>
-          <span>{x.message}</span>
-        </div>
-      ))}
+      {messages.map((x, i) =>
+        isLogArgs(x) ? (
+          // Normal log entry
+          <div key={i}>
+            <span
+              style={{
+                color: x.color,
+                marginRight: 2,
+              }}
+            >
+              {x.namespace}
+            </span>
+            <span>{x.message}</span>
+          </div>
+        ) : (
+          // Error
+          <div key={i}>
+            <span
+              style={{
+                color: theme.syntax.error.hex(),
+              }}
+            >
+              {x.message}
+            </span>
+          </div>
+        )
+      )}
     </Wrapper>
   );
 }
