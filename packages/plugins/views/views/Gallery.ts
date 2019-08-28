e0407273d0a7880ea24653199793924b8dbb3d01
Lynn Smeria <ae@cephea.de>
2019-08-28 11:50:11 +0200

    Gallery: handle null values

--- packages/plugins/views/views/Gallery.ts
+++ packages/plugins/views/views/Gallery.ts
@@ -14,7 +14,10 @@ export interface ViewState {
 }
 
 export const Gallery: CocoonView<ViewData, ViewState> = {
-  description: `Shows a gallery of remote images.`,
+  description: `Shows a gallery of remote images.
+
+  Expects to receive either an array of URLs, or an array of objects in the shape of \`{ src: string, title?: string }\``,
+
   stateDescriptions: {
     limit: `Limits the number of images shown. (default: \`50\`)`,
     size: `Height of the gallery images.`,
@@ -22,8 +25,8 @@ export const Gallery: CocoonView<ViewData, ViewState> = {
 
   serialiseViewData: async (context, data: any[], state) => {
     const limit = state.limit === undefined ? 50 : state.limit;
-    const filteredData = data.filter(x =>
-      typeof x === 'string' ? Boolean(x) : Boolean(x.src)
+    const filteredData = data.filter(
+      x => Boolean(x) && (typeof x === 'string' ? Boolean(x) : Boolean(x.src))
     );
     return limit ? filteredData.slice(0, limit) : filteredData;
   },
