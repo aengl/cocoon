aa9edacdbb95b21145dd15ea066d2372f03e25fb
Lynn Smeria <ae@cephea.de>
2019-09-02 14:52:53 +0200

    Pipe: refactor, add documentation & improve errors

--- packages/cocoon/src/nodes/io/Pipe.ts
+++ packages/cocoon/src/nodes/io/Pipe.ts
@@ -1,49 +1,72 @@
 import { CocoonNode } from '@cocoon/types';
+import castFunction from '@cocoon/util/castFunction';
 import { spawnSync } from 'child_process';
 
 export interface Ports {
   command: string;
   data: object[];
-  json?: boolean;
+  serialise?: string | ((x: any) => any);
+  deserialise?: string | ((x: any) => any);
 }
 
 export const Pipe: CocoonNode<Ports> = {
   category: 'I/O',
-  description: `Pipes an entire collection (optionally encoded as JSON) into a terminal command via stdin, and reads the result back from stdout. The process needs to return the entire collection as either JSON, or split by newlines.`,
+  description: `Pipes an entire collection into a terminal command via stdin, and reads the result back from stdout.`,
 
   in: {
     command: {
+      description: `The shell command to execute.`,
       required: true,
       visible: false,
     },
     data: {
-      required: true,
+      description: `The data that is piped into the command via stdin.`,
     },
-    json: {
+    deserialise: {
+      description: `A function that deserialises the data that is read back from stdout. If unspecified, it will be a string.`,
+      visible: false,
+    },
+    serialise: {
+      description: `A function that serialises the data that is piped into the process via stdin. If unspecified, \`toString()\` will be called on the data object.`,
       visible: false,
     },
   },
 
   out: {
-    data: {},
+    data: {
+      description: `The data read back from stdout.`,
+    },
   },
 
   async *process(context) {
-    const { command, data, json } = context.ports.read();
+    const { command, data, deserialise, serialise } = context.ports.read();
     context.debug(`executing "${command}"`);
+    const stdin = serialise ? castFunction(serialise)(data) : '';
     const result = spawnSync(command, {
       cwd: context.cocoonFile.root,
-      input: json ? JSON.stringify(data) : data.toString(),
+      input: data
+        ? serialise
+          ? castFunction(serialise)(data)
+          : data.toString()
+        : undefined,
+      shell: true,
     });
     if (result.error) {
       throw result.error;
     }
+    if (result.status !== 0) {
+      throw new Error(
+        `process returned with status ${result.status}
+
+${result.stderr.toString()}`
+      );
+    }
     context.ports.write({
       data:
         result.stdout.length > 0
-          ? json
-            ? JSON.parse(result.stdout.toString())
-            : result.stdout.toString().split('\n')
+          ? deserialise
+            ? castFunction(deserialise)(result.stdout.toString())
+            : result.stdout.toString()
           : null,
     });
   },
